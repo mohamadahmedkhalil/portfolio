ead546a4c15f90251deb9fb7087ad2376e5234f4
mohamadahmedkhalil <111133358+mohamadahmedkhalil@users.noreply.github.com>
2026-03-08 16:04:44 +0100

    Add files via upload

---
+++ pages/portfolio/projects-data.js
@@ -0,0 +1,57 @@
+const portfolioData = {
+  profile: {
+    name: "MOHAMAD KHALIL",
+    title: "Maschinenbau Student an der TU Berlin",
+    email: "mohamadahmedkhalil@gmail.com",
+    phone: "+49 176 70050922",
+    linkedinUrl: "https://www.linkedin.com/in/mohamadahmedkhalil/",
+    linkedinText: "linkedin.com/in/mohamadahmedkhalil"
+  },
+
+  projects: [
+    {
+      title: "Bachelorarbeit – Konstruktion und Aufbau eines Baggerarms",
+      link: "../projects/bachelorarbeit/index.html",
+      items: [
+        {
+          image: "img/bachelorarbeit-1.jpg",
+          alt: "Konzept des Baggerarms",
+          text: "Konzept und erster Entwurf des Baggerarms."
+        },
+        {
+          image: "img/bachelorarbeit-2.jpg",
+          alt: "CAD-Modell des Baggerarms",
+          text: "CAD-Modell und konstruktive Auslegung der Baugruppen."
+        },
+        {
+          image: "img/bachelorarbeit-3.jpg",
+          alt: "Finaler Aufbau",
+          text: "Finaler Aufbau für den Einsatz in der Lehre."
+        }
+      ]
+    },
+    {
+      title: "Projekt Mehrkörperdynamik – Fahrwerk-Simulation eines Motocross-Motorrads",
+      link: "../projects/mehrkoerperdynamik/index.html",
+      items: [
+        {
+          image: "img/mkd-1.jpg",
+          alt: "Modellaufbau",
+          text: "Aufbau des Mehrkörpersystems und Definition der Komponenten."
+        },
+        {
+          image: "img/mkd-2.jpg",
+          alt: "Simulation",
+          text: "Simulation des Fahrwerks unter dynamischer Belastung."
+        },
+        {
+          image: "img/mkd-3.jpg",
+          alt: "Ergebnisse",
+          text: "Auswertung der Schwingungs- und Bewegungsantworten."
+        }
+      ]
+    }
+
+    
+  ]
+};
